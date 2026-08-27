f666171d92b371152680394cd75972c0c6c32350
dssreddy428 <dssreddy428@gmail.com>
2026-08-27 13:11:51 +0530

    AugResolution

---
+++ src/augRev/Even.java
@@ -0,0 +1,17 @@
+package augRev;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class Even {
+	
+	public static void main(String[] args)
+	{
+		List<Integer> ls = Arrays.asList(1,2,3,4,5,6,7,8);
+		
+		ls.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+		
+		
+	}
+
+}
---
+++ src/augRev/Even1.java
@@ -0,0 +1,18 @@
+package augRev;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Even1 {
+	
+	public static void main(String[] args)
+	{
+		List<Integer> ls = Arrays.asList(1,2,3,4,5,6);
+		
+		List<Integer>  eve = ls.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
+		
+		System.out.println(eve);
+	}
+
+}
