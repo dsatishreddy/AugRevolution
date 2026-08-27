9f731221f6b095a9b46abc38ea8f0d12749e0f22
dssreddy428 <dssreddy428@gmail.com>
2026-08-27 13:46:54 +0530

    Initial commit

---
+++ src/augRev/SumOfEvenNumbers.java
@@ -0,0 +1,21 @@
+package augRev;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumOfEvenNumbers {
+
+	public static void main(String[] args)
+	{
+		List<Integer> ls = Arrays.asList(1,2,3,4,5,6,7,8);
+		
+	 int lt = ls.stream()
+			    .filter(n -> n%2 == 0)
+		        .mapToInt(Integer::intValue)
+		        .sum();
+	 
+	 System.out.println("Even Sum : " + lt);
+	
+	
+	}
+}
